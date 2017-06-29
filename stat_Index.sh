9b841aa5ca0fad419fdf92697d1f42bc8eb7d359
Min Young Lee <minyoung.lee@gmail.com>
2017-06-29 09:16:01 -0700

    Add files via upload

---
+++ stat_Index.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# list of fastq files (split fastq files from different lanes)
+fastq=`find -maxdepth 1 -name '*.fastq' | sort`
+echo "$fastq" | cut -d "/" -f2 > list_fastq.txt
+
+# parse index sequences
+for input in `cat ./list_fastq.txt`
+do
+
+# display the processing file name
+echo $input
+
+# output file name
+output=`echo $input | cut -d '.' -f1`
+output="$output.indexSeq"
+
+# parse all index sequences and counts
+grep "@" ${input} | cut -d ":" -f10 | sort | uniq -c | sort -n -r > $output
+N=`sed -n '1,1p' $output | cut -d " " -f2` 
+S=`wc -l ${input} | cut -d " " -f1`
+let S=S/4
+R=`echo "scale=4; $N/$S" | bc`
+echo -e "${input}\t$S\t$N\t$R" >> Stat_index.txt 
+
+done
