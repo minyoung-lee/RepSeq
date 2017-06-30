b24b31efa921438055396d25b2e8756c9d5fe5fe
Min Young Lee <minyoung.lee@gmail.com>
2017-06-30 09:24:54 -0700

    Update run_mixcr.sh

--- run_mixcr.sh
+++ run_mixcr.sh
@@ -1,10 +1,5 @@
 #! /bin/bash
 
-flag_TCRb=0
-flag_IGHG=0
-flag_IGKC=0
-flag_IGLC=0
-
 # list of fastq files (trimmed fastq files)
 fastq=`find ./cutadapt  -name '*.fastq' | sort`
 echo "$fastq" | cut -d "/" -f3 | cut -d "_" -f1,2,3,4 | uniq > list_fastq.txt
@@ -23,6 +18,25 @@ flag_TCRb=`expr $gene == "TCRb"`
 flag_IGHG=`expr $gene == "IGHG"`
 flag_IGKC=`expr $gene == "IGKC"`
 flag_IGLC=`expr $gene == "IGLC"`
+if ((flag_TCRb==1))
+then
+gene="TRB"
+fi
+
+if ((flag_IGHG==1))
+then
+gene="IGH"
+fi
+
+if ((flag_IGKC==1))
+then
+gene="IGK"
+fi
+
+if ((flag_IGLC==1))
+then
+gene="IGL"
+fi
 
 # file names
 R1_input="./cutadapt/${input}_R1_001.fastq"
@@ -34,37 +48,10 @@ assembleReport="assembleReport_${input}.log"
 clone="clones_${input}.clns"
 cloneTxt="clones_${input}.txt"
 
-if ((flag_TCRb==1))
-then
-mixcr align --species hs --loci TRB -OvParameters.geneFeatureToAlign=VTranscript --report $alignmentReport $R1_input $R2_input $alignment
+mixcr align --species hs --loci $gene -OvParameters.geneFeatureToAlign=VTranscript --report $alignmentReport $R1_input $R2_input $alignment
 mixcr assemble --report $assembleReport $alignment $clone
 mixcr exportAlignments $alignment $alignmentTxt
 mixcr exportClones -nFeature CDR3 $clone $cloneTxt
-fi
-
-if ((flag_IGHG==1))
-then
-mixcr align --species hs --loci IGH -OvParameters.geneFeatureToAlign=VTranscript --report $alignmentReport $R1_input $R2_input $alignment
-mixcr assemble --report $assembleReport $alignment $clone
-mixcr exportAlignments $alignment $alignmentTxt
-mixcr exportClones -nFeature CDR3 $clone $cloneTxt
-fi
-
-if ((flag_IGKC==1))
-then
-mixcr align --species hs --loci IGK -OvParameters.geneFeatureToAlign=VTranscript --report $alignmentReport $R1_input $R2_input $alignment
-mixcr assemble --report $assembleReport $alignment $clone
-mixcr exportAlignments $alignment $alignmentTxt
-mixcr exportClones -nFeature CDR3 $clone $cloneTxt
-fi
-
-if ((flag_IGLC==1))
-then
-mixcr align --species hs --loci IGL -OvParameters.geneFeatureToAlign=VTranscript --report $alignmentReport $R1_input $R2_input $alignment
-mixcr assemble --report $assembleReport $alignment $clone
-mixcr exportAlignments $alignment $alignmentTxt
-mixcr exportClones -nFeature CDR3 $clone $cloneTxt
-fi
 
 done
 mkdir mixcr_align
